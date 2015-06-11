05bad6114d63285dbcc0049849e794a8e1b319d0
Jim <pringlized@gmail.com>
2015-06-10 23:10:48 -0700

    Updated Program.cs to use System.Security.Cryptology lib

--- Phraseware/Program.cs
+++ Phraseware/Program.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace Phraseware
 {
 	class MainClass
 	{
+		private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+		Dictionary<string, string> wordListTemp = new Dictionary<string, string> ();
+
+		private static byte numSides = 6;
+		private static byte minNumWords = 5;
+		private static byte maxNumWords = 12;
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Let's make a Diceware inspired password...");
@@ -20,20 +29,22 @@ namespace Phraseware
 
 			int numWords;
 			int.TryParse (consoleInput, out numWords);
-			while ( !(numWords >= 5) || !(numWords <= 12) ) {
-				Console.WriteLine("Please enter a number between 5 and 12:");
+			while ( !(numWords >= minNumWords) || !(numWords <= maxNumWords) ) 
+			{
+				Console.WriteLine("Please enter a number between " + minNumWords + " and " + maxNumWords + ":");
 				consoleInput = Console.ReadLine();
 				int.TryParse (consoleInput, out numWords);
 			}
 
-			// generateWordKeys
+			// generate keys to find word in word list
 			string[] wordKeys = generateKeys(wordList, numWords);
 
 			// Get Words
 			string thePhrase = getPhrase(wordList, wordKeys);
 
 			// echo out to console
-			Console.WriteLine("Your phrase: " + thePhrase);
+			Console.WriteLine ("Your phrase: " + thePhrase);
+			Console.WriteLine ("Phrase length: " + thePhrase.Length + " characters");
 		}
 
 		private static Dictionary<string, string> readDictionaryFile(Dictionary<string, string> wordList, string fileName)
@@ -58,15 +69,15 @@ namespace Phraseware
 		{
 			string[] wordKeys = new string[numWords];
 
-			for (var i = 0; i < numWords; ++i) {
+			for (var i = 0; i < numWords; ++i) 
+			{
 				string theWord = "";
 
 				// roll the dice 5 times
-				for (var r = 1; r < 6; ++r) {
-					// Random should not be placed here, but using GUID currently to seed a decent randomness)
-					Random rnd = new System.Random (Guid.NewGuid().GetHashCode());
-					int roll = rnd.Next(1, 6);
-					theWord += roll;
+				for (var r = 1; r < numSides; ++r) 
+				{
+					byte rolledNum = RollDice(numSides);
+					theWord += System.Convert.ToString(rolledNum);
 				}
 
 				// add new number to array of wordKeys
@@ -76,10 +87,35 @@ namespace Phraseware
 			return wordKeys;
 		}
 
+		// get random number from cryptology class
+		public static byte RollDice(byte numSides) 
+		{
+			// array holding random value
+			byte[] randomNumber = new byte[1];
+
+			do 
+			{
+				// get a random value
+				rngCsp.GetBytes(randomNumber);
+			} 
+			while(!isFairRoll(randomNumber[0], numSides));
+
+			return (byte)((randomNumber [0] % numSides) + 1);
+		}
+
+		//TODO: write explantion for how this works
+		public static bool isFairRoll(byte roll, byte numSides)
+		{
+			int fullSetOfValues = Byte.MaxValue / numSides;
+			return roll < numSides * fullSetOfValues;
+		}
+
+		// match up each word dice roll to word in dictionary
 		private static string getPhrase(Dictionary<string, string> wordList, string[] wordKeys)
 		{
 			string thePhrase = "";
-			foreach (string key in wordKeys) {
+			foreach (string key in wordKeys) 
+			{
 				thePhrase += " " + wordList [key];
 			}
 			return thePhrase;
