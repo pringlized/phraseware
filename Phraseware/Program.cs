3f0b44653116892a3b5567280e327796ed4a71eb
Jim <pringlized@gmail.com>
2015-06-10 23:49:53 -0700

    Mopping up variable names

--- Phraseware/Program.cs
+++ Phraseware/Program.cs
@@ -9,22 +9,24 @@ namespace Phraseware
 	class MainClass
 	{
 		private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
-
-		Dictionary<string, string> wordListTemp = new Dictionary<string, string> ();
+		private static Dictionary<string, string> dictList = new Dictionary<string, string> ();
 
 		private static byte numSides = 6;
 		private static byte minNumWords = 5;
 		private static byte maxNumWords = 12;
 
+		private static string dictionaryFileName = "dictionary.eng.csv";
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Let's make a Diceware inspired password...");
+			Console.WriteLine ("------------------------------------------");
 
-			// read wordList CSV file into array
-			Dictionary<string, string> wordList = readDictionaryFile(new Dictionary<string, string> (), "word_list.csv");
+			// read dictionary CSV file into array
+			LoadDictionaryFile();
 
-			// ask how many words
-			Console.WriteLine("How many words do you want to generate (between 5 & 12):");
+			// ask how many words within a rango
+			Console.WriteLine("How many words do you want to generate (" + minNumWords + " and " + maxNumWords + "):");
 			string consoleInput = Console.ReadLine ();
 
 			int numWords;
@@ -37,20 +39,20 @@ namespace Phraseware
 			}
 
 			// generate keys to find word in word list
-			string[] wordKeys = generateKeys(wordList, numWords);
+			string[] wordKeys = GenerateKeys(numWords);
 
 			// Get Words
-			string thePhrase = getPhrase(wordList, wordKeys);
+			string thePhrase = GetPhrase(wordKeys);
 
 			// echo out to console
-			Console.WriteLine ("Your phrase: " + thePhrase);
+			Console.WriteLine ("\nYour phrase: " + thePhrase);
 			Console.WriteLine ("Phrase length: " + thePhrase.Length + " characters");
 		}
 
-		private static Dictionary<string, string> readDictionaryFile(Dictionary<string, string> wordList, string fileName)
+		private static void LoadDictionaryFile()
 		{
 			string line;
-			var filestream = new FileStream(fileName,
+			var filestream = new FileStream(dictionaryFileName,
 				FileMode.Open,
 				FileAccess.Read,
 				FileShare.ReadWrite);
@@ -59,22 +61,20 @@ namespace Phraseware
 			while ((line = file.ReadLine()) != null)
 			{
 				string[] values = line.Split (',');
-				wordList.Add (values[0], values[1]);
+				dictList.Add (values[0], values[1]);
 			}
-
-			return wordList;
 		}
 
-		private static string[] generateKeys(Dictionary<string, string> wordList, int numWords)
+		private static string[] GenerateKeys(int numWords)
 		{
 			string[] wordKeys = new string[numWords];
 
-			for (var i = 0; i < numWords; ++i) 
+			for (int i = 0; i < numWords; ++i) 
 			{
 				string theWord = "";
 
-				// roll the dice 5 times
-				for (var r = 1; r < numSides; ++r) 
+				// roll the dice
+				for (int r = 1; r < numSides; ++r) 
 				{
 					byte rolledNum = RollDice(numSides);
 					theWord += System.Convert.ToString(rolledNum);
@@ -87,7 +87,10 @@ namespace Phraseware
 			return wordKeys;
 		}
 
+		// <summary>
 		// get random number from cryptology class
+		// </summary>
+
 		public static byte RollDice(byte numSides) 
 		{
 			// array holding random value
@@ -98,25 +101,27 @@ namespace Phraseware
 				// get a random value
 				rngCsp.GetBytes(randomNumber);
 			} 
-			while(!isFairRoll(randomNumber[0], numSides));
+			while(!IsFairRoll(randomNumber[0], numSides));
 
 			return (byte)((randomNumber [0] % numSides) + 1);
 		}
-
-		//TODO: write explantion for how this works
-		public static bool isFairRoll(byte roll, byte numSides)
+			
+		public static bool IsFairRoll(byte roll, byte numSides)
 		{
 			int fullSetOfValues = Byte.MaxValue / numSides;
 			return roll < numSides * fullSetOfValues;
 		}
 
+		// <summary>
 		// match up each word dice roll to word in dictionary
-		private static string getPhrase(Dictionary<string, string> wordList, string[] wordKeys)
+		// </summary>
+
+		private static string GetPhrase(string[] wordKeys)
 		{
 			string thePhrase = "";
 			foreach (string key in wordKeys) 
 			{
-				thePhrase += " " + wordList [key];
+				thePhrase += " " + dictList [key];
 			}
 			return thePhrase;
 		}
